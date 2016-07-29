3ed979579a27d49169e5b808ae699a21ed165521
xumanyu <manyuemily@sina.com>
2016-07-30 06:58:41 +0800

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
  resources :users 
+ 
+ resources :microposts, only: [:create, :destroy]    
   # You can have the root of your site routed with "root"
  root 'static_pages#home'
 
@@ -20,6 +22,8 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
+
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
