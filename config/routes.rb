3ec697e215273ef778d4cb4e76a3d533898f791f
xumanyu <manyuemily@sina.com>
2016-07-30 01:18:06 +0800

    Skeleton User profile page support

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
    
-  get 'users/new'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+ resources :users 
   # You can have the root of your site routed with "root"
  root 'static_pages#home'
 
@@ -16,7 +14,7 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
-  get 'users/new'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
